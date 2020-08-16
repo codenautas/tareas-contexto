c0f7a1fd0a5a9122c90430623146f07bda1a2223
Emilio Platzer <emilioplatzer@gmail.com>
2020-08-16 17:05:55 -0300

    cuenta la cantidad de tareas pendientes, terminadas, etc...

--- src/server/table-contextos.ts
+++ src/server/table-contextos.ts
@@ -12,10 +12,34 @@ export function contextos(_context:TableContext):TableDefinition{
             {name:'nombre'           , typeName:'text'    , isName:true     },
             {name:'activo'           , typeName:'boolean' , nullable:false ,defaultValue:true},
             {name:'objetivo'         , typeName:'text'                      },
+            {name:'sin_empezar'      , typeName:'bigint'  , editable:false  },
+            {name:'empezadas'        , typeName:'bigint'  , editable:false  },
+            {name:'terminadas'       , typeName:'bigint'  , editable:false  },
         ],
         primaryKey:['contexto'],
         detailTables:[
-            {table:'tareas', fields:['contexto'], abr:'T'}
-        ]
+            {table:'tareas', fields:['contexto'], abr:'T', refreshParent:true}
+        ],
+        sql:{
+            isTable:true,
+            from:`(
+                select * 
+                    from contextos c,
+                        lateral (
+                            select count(*) as cantidad_tareas,
+                                    count(*) filter (where estado='sin_empezar') as sin_empezar,
+                                    count(*) filter (where estado='empezada') as empezadas,
+                                    count(*) filter (where estado='terminada') as terminadas
+                                from (select t.*, 
+                                    case 
+                                        when t.terminada then 'terminada'
+                                        when t.empezada then 'empezada'
+                                        else 'sin_empezar'
+                                    end as estado
+                                        from tareas t where c.contexto=t.contexto
+                                ) te -- tareas caracterizadas por su estado
+                        ) x
+            )`
+        }
     };
 }
